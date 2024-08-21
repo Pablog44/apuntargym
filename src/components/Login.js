d4a66e1868fa60eb07bdb49675370bf193691e4b
PABLOGONZALEZCUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-08-21 04:14:12 +0200

    cambia botón por icono al cerrar sesión, y otros estilos ademas de que obliga a elegir cuenta de google.
    -obliga elegir cuenta de google
    -Cambia botón de cerrar sesión por icono svg de puerta
    -Se cambia algún estilo.

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,37 +3,43 @@ import React, { useEffect } from 'react';
 import { auth, provider } from '../firebase';
 import { signInWithPopup, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import '../App.css';
 
 function Login() {
     const navigate = useNavigate();
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // Si el usuario ya está autenticado, redirigir al panel principal
-            navigate('/registrar');
-        }
+            if (user) {
+                // Si el usuario ya está autenticado, redirigir al panel principal
+                navigate('/registrar');
+            }
         });
     }, [navigate]);
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
-        .then((result) => {
-            console.log('Autenticación exitosa:', result.user);
-            navigate('/registrar');
-        })
-        .catch((error) => {
-            console.error('Error durante la autenticación:', error.message);
-            alert(`Error de autenticación: ${error.message}`);
+        // Configuración para forzar el selector de cuentas de Google
+        const customProvider = provider.setCustomParameters({
+            prompt: 'select_account' // Fuerza el selector de cuentas
         });
+
+        signInWithPopup(auth, customProvider)
+            .then((result) => {
+                console.log('Autenticación exitosa:', result.user);
+                navigate('/registrar');
+            })
+            .catch((error) => {
+                console.error('Error durante la autenticación:', error.message);
+                alert(`Error de autenticación: ${error.message}`);
+            });
     };
 
     return (
         <div className="login-container">
-        <h2>Inicia sesión con tu cuenta de Google</h2>
-        <button onClick={handleGoogleSignIn}>Iniciar Sesión con Google</button>
+            <h2>Inicia sesión con tu cuenta de Google</h2>
+            <button onClick={handleGoogleSignIn}>Iniciar Sesión con Google</button>
         </div>
     );
-    }
+}
 
 export default Login;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ import './Sidebar.css';
 
 const Sidebar = () => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const navigate = useNavigate(); // Necesario para redirigir después de cerrar sesión
+  const navigate = useNavigate();
 
   const toggleSidebar = () => {
     setIsExpanded(!isExpanded);
@@ -28,20 +28,17 @@ const Sidebar = () => {
 
   return (
     <div>
-      {/* Ícono de hamburguesa visible solo cuando el menú está cerrado */}
       {!isExpanded && (
         <button className="hamburger-icon" onClick={toggleSidebar}>
           &#9776;
         </button>
       )}
 
-      {/* Barra lateral */}
       <div className={isExpanded ? "sidebar expanded" : "sidebar"}>
         <button className="close-button" onClick={toggleSidebar}>
-          {/* SVG para la "X" */}
           <svg viewBox="0 0 24 24">
-            <line x1="18" y1="6" x2="6" y2="18" stroke="currentColor" strokeWidth="2"/>
-            <line x1="6" y1="6" x2="18" y2="18" stroke="currentColor" strokeWidth="2"/>
+            <line x1="18" y1="6" x2="6" y2="18" stroke="currentColor" strokeWidth="2" />
+            <line x1="6" y1="6" x2="18" y2="18" stroke="currentColor" strokeWidth="2" />
           </svg>
         </button>
         <nav className="sidebar-nav" onClick={closeSidebar}>
@@ -51,9 +48,18 @@ const Sidebar = () => {
           <Link to="/resultados">Resultados</Link>
           <Link to="/añadir">Añadir Grupos/Ejercicios</Link>
         </nav>
-        {/* Botón de cerrar sesión, que siempre está al final */}
         <div className="logout-container">
-          <button onClick={handleLogout}>Cerrar Sesión</button>
+          <div className="logout-circle" onClick={handleLogout}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="100"
+              height="100"
+              viewBox="0 0 256 256"
+              fill="currentColor"
+            >
+              <path d="M69.313 54.442a2 2 0 01-1.147-.363 2.002 2.002 0 01-.487-2.786l10.118-14.399L67.679 22.495a2 2 0 012.786-2.786l10.926 15.549a2 2 0 010 2.3L70.952 53.592a2.003 2.003 0 01-1.639.85zM57.693 30.092a2 2 0 002-2V2a2 2 0 00-2-2H9.759a2 2 0 00-1.74.978 2 2 0 00-.258 1.83V72.787c0 .558.22 1.083.614 1.478.395.395.92.614 1.478.614h31.016a2 2 0 002-2v-17h14.918a2 2 0 002-2V45a2 2 0 10-4 0v20.787H42.775V18.213a2 2 0 00-1.074-1.772L17.902 4h37.791V28.092a2 2 0 002 2zM80.241 38.894H47.536a2 2 0 110-4h32.705a2 2 0 110 4z" />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
