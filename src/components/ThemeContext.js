a25a6188e5d930141f1017183c75dac8108172aa
PABLOGONZALEZCUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-08-22 04:28:24 +0200

    Estilos para solucionar problema móvil modo oscuro
    -Estilos para solucionar problema móvil modo oscuro

--- src/components/ThemeContext.js
+++ src/components/ThemeContext.js
@@ -13,7 +13,13 @@ export const ThemeProvider = ({ children }) => {
   useEffect(() => {
     // Almacena el tema seleccionado en localStorage
     localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-    document.documentElement.className = isDarkMode ? 'dark-mode' : ''; // Aplica la clase a todo el documento
+    document.documentElement.className = isDarkMode ? 'dark-mode' : '';
+
+    // Cambiar dinámicamente el meta `theme-color`
+    const themeColorMeta = document.querySelector('meta[name="theme-color"]');
+    if (themeColorMeta) {
+      themeColorMeta.setAttribute('content', isDarkMode ? '#121212' : '#f0f0f0');
+    }
   }, [isDarkMode]);
 
   const toggleTheme = () => {
