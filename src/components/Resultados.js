78ee2f8e83bedada17dd7105bd45bcb8e68ef9c3
PABLOGONZALEZCUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-08-29 05:23:43 +0200

    se añade criterio pr en los resultados.
    -se añade criterio pr en los resultados.

--- src/components/Resultados.js
+++ src/components/Resultados.js
@@ -45,19 +45,33 @@ function Resultados() {
 
   const sortAndDisplayResults = useCallback(
     (records) => {
-      const sortedRecords = [...records].sort((a, b) => {
-        if (sortCriteria === 'dateTime') {
-          return new Date(b.dateTime) - new Date(a.dateTime);
-        } else if (sortCriteria === 'weight') {
-          if (b.weight === a.weight) {
-            return b.repetitions - a.repetitions; // Ordena por repeticiones si el peso es el mismo
+      let sortedRecords;
+
+      if (sortCriteria === 'PR') {
+        const groupedByExercise = records.reduce((acc, record) => {
+          const key = `${record.exercise}-${record.weight}`;
+          if (!acc[key] || acc[key].repetitions < record.repetitions) {
+            acc[key] = record;
           }
-          return b.weight - a.weight; // Ordena por peso de mayor a menor
-        } else if (sortCriteria === 'repetitions') {
-          return b.repetitions - a.repetitions;
-        }
-        return 0;
-      });
+          return acc;
+        }, {});
+
+        sortedRecords = Object.values(groupedByExercise).sort((a, b) => b.weight - a.weight);
+      } else {
+        sortedRecords = [...records].sort((a, b) => {
+          if (sortCriteria === 'dateTime') {
+            return new Date(b.dateTime) - new Date(a.dateTime);
+          } else if (sortCriteria === 'weight') {
+            if (b.weight === a.weight) {
+              return b.repetitions - a.repetitions; // Ordena por repeticiones si el peso es el mismo
+            }
+            return b.weight - a.weight; // Ordena por peso de mayor a menor
+          } else if (sortCriteria === 'repetitions') {
+            return b.repetitions - a.repetitions;
+          }
+          return 0;
+        });
+      }
 
       setFilteredRecords(sortedRecords);
     },
@@ -85,53 +99,54 @@ function Resultados() {
     <div className="resultados-container card-container">
       <h1 className="resultados-title">Resultados</h1>
       <div className="form-container">
-      <label htmlFor="filter-muscle-group">Grupo Muscular:</label>
-      <select
-        id="filter-muscle-group"
-        value={selectedGroup}
-        onChange={(e) => setSelectedGroup(e.target.value)}
-      >
-        <option value="">Todos</option>
-        {[...new Set(exerciseRecords.map((record) => record.muscleGroup))].map((group) => (
-          <option key={group} value={group}>
-            {group}
-          </option>
-        ))}
-      </select>
-
-      <label htmlFor="filter-exercise">Ejercicio:</label>
-      <select
-        id="filter-exercise"
-        value={selectedExercise}
-        onChange={(e) => setSelectedExercise(e.target.value)}
-        disabled={!selectedGroup}
-      >
-        <option value="">Todos</option>
-        {exerciseRecords
-          .filter((record) => record.muscleGroup === selectedGroup)
-          .map((record) => record.exercise)
-          .filter((exercise, index, self) => self.indexOf(exercise) === index)
-          .map((exercise) => (
-            <option key={exercise} value={exercise}>
-              {exercise}
+        <label htmlFor="filter-muscle-group">Grupo Muscular:</label>
+        <select
+          id="filter-muscle-group"
+          value={selectedGroup}
+          onChange={(e) => setSelectedGroup(e.target.value)}
+        >
+          <option value="">Todos</option>
+          {[...new Set(exerciseRecords.map((record) => record.muscleGroup))].map((group) => (
+            <option key={group} value={group}>
+              {group}
             </option>
           ))}
-      </select>
+        </select>
 
-      <button className="apply-button" onClick={applyFilters}>Aplicar Filtros</button>
+        <label htmlFor="filter-exercise">Ejercicio:</label>
+        <select
+          id="filter-exercise"
+          value={selectedExercise}
+          onChange={(e) => setSelectedExercise(e.target.value)}
+          disabled={!selectedGroup}
+        >
+          <option value="">Todos</option>
+          {exerciseRecords
+            .filter((record) => record.muscleGroup === selectedGroup)
+            .map((record) => record.exercise)
+            .filter((exercise, index, self) => self.indexOf(exercise) === index)
+            .map((exercise) => (
+              <option key={exercise} value={exercise}>
+                {exercise}
+              </option>
+            ))}
+        </select>
 
-      <h2>Ordenar Resultados</h2>
-      <label htmlFor="sort-criteria">Criterio:</label>
-      <select
-        id="sort-criteria"
-        value={sortCriteria}
-        onChange={(e) => setSortCriteria(e.target.value)}
-      >
-        <option value="weight">Peso</option>
-        <option value="repetitions">Repeticiones</option>
-        <option value="dateTime">Fecha</option>
-      </select>
-</div>
+        <button className="apply-button" onClick={applyFilters}>Aplicar Filtros</button>
+
+        <h2>Ordenar Resultados</h2>
+        <label htmlFor="sort-criteria">Criterio:</label>
+        <select
+          id="sort-criteria"
+          value={sortCriteria}
+          onChange={(e) => setSortCriteria(e.target.value)}
+        >
+          <option value="weight">Peso</option>
+          <option value="repetitions">Repeticiones</option>
+          <option value="dateTime">Fecha</option>
+          <option value="PR">PR</option> {/* Nueva opción añadida */}
+        </select>
+      </div>
       <ul className="results-list">
         {filteredRecords.length === 0 ? (
           <li className="no-results">No se encontraron resultados.</li>
